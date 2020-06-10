b1691c9a2ca6e5e3b3a310e99954618395c83300
LimitToInfinity <21stLimitless@gmail.com>
2020-06-10 12:46:02 -0600

    Displays dogs from backend onto frontend index.html.

--- dogs_app/index.js
+++ dogs_app/index.js
@@ -3,14 +3,27 @@ console.log("JavaScript loaded");
 baseURL = "http://localhost:3000";
 dogsURL = `${baseURL}/dogs`;
 
+const dogsSection = document.querySelector("section");
+
 fetch(dogsURL)
   .then(parseJSON)
   .then(displayDogs);
 
 function displayDogs(dogs) {
-  console.log("dogs", dogs);
+  dogs.forEach(showDog)
+}
 
+function showDog(dog) {
+  const dogCard = document.createElement("div");
+  
+  const name = document.createElement("h2");
+  name.textContent = dog.name; 
+  
+  const age = document.createElement("p");
+  age.textContent = dog.age;
 
+  dogCard.append(name, age);
+  dogsSection.append(dogCard);
 }
 
 function parseJSON(response) {
