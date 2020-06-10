840bada0cc9f606a2bcd521d1dfbd348692ea54d
LimitToInfinity <21stLimitless@gmail.com>
2020-06-10 12:41:15 -0600

    Adds cors in gemfile and cors.rb. Adds JS to fetch dogs -no pun intended.

--- dogs_app/index.js
+++ dogs_app/index.js
@@ -1,1 +1,18 @@
 console.log("JavaScript loaded");
+
+baseURL = "http://localhost:3000";
+dogsURL = `${baseURL}/dogs`;
+
+fetch(dogsURL)
+  .then(parseJSON)
+  .then(displayDogs);
+
+function displayDogs(dogs) {
+  console.log("dogs", dogs);
+
+
+}
+
+function parseJSON(response) {
+  return response.json()
+}
